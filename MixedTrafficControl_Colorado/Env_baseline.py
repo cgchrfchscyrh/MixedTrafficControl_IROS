f5f3bee2a01dd30adee96e645b5081dbdca2abc5
Altair <liusongyang@ufl.edu>
2025-01-02 12:37:02 -0500

    Update Env_baseline.py

--- MixedTrafficControl_Colorado/Env_baseline.py
+++ MixedTrafficControl_Colorado/Env_baseline.py
@@ -168,18 +168,26 @@ class Env(MultiAgentEnv):
         self.queue_waiting_time = dict()
         self.head_of_control_queue = dict()
         self.inner_speed = dict()
+
+        for JuncID in all_junction_list:
+            self.queue[JuncID] = dict()
+            self.queue_waiting_time[JuncID] = dict()
+            for keyword in self.keywords_order:
+                self.queue[JuncID][keyword] = []
+                self.queue_waiting_time[JuncID][keyword] = []
+
         for JuncID in self.junction_list:
             self.control_queue[JuncID] = dict()
             self.control_queue_waiting_time[JuncID] = dict()
-            self.queue[JuncID] = dict()
-            self.queue_waiting_time[JuncID] = dict()
+            # self.queue[JuncID] = dict()
+            # self.queue_waiting_time[JuncID] = dict()
             self.head_of_control_queue[JuncID] = dict()
             self.inner_speed[JuncID] = []
             for keyword in self.keywords_order:
                 self.control_queue[JuncID][keyword] = []
                 self.control_queue_waiting_time[JuncID][keyword] = []
-                self.queue[JuncID][keyword] = []
-                self.queue_waiting_time[JuncID][keyword] = []
+                # self.queue[JuncID][keyword] = []
+                # self.queue_waiting_time[JuncID][keyword] = []
                 self.head_of_control_queue[JuncID][keyword] = []
 
         ## global reward related        
@@ -475,7 +483,8 @@ class Env(MultiAgentEnv):
                 if self.map.get_distance_to_intersection(veh)<=self.control_zone_length:
                     self.queue[JuncID][keyword].extend([veh])
                     self.queue_waiting_time[JuncID][keyword].extend([self.veh_waiting_juncs[veh.id][JuncID]])
-                    if veh.type == 'RL':
+                    
+                    if veh.type == 'RL' and JuncID in self.junction_list:
                         self.control_queue[JuncID][keyword].extend([veh])
                         self.control_queue_waiting_time[JuncID][keyword].extend([self.veh_waiting_juncs[veh.id][JuncID]])
 
