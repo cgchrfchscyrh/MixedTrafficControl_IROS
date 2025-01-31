7caa76f1ede45edb31827e4af00e500e3802b857
Altair <liusongyang@ufl.edu>
2025-01-30 22:21:27 -0500

    Update Env_smaller.py

--- MixedTrafficControl_Colorado/Env_smaller.py
+++ MixedTrafficControl_Colorado/Env_smaller.py
@@ -99,7 +99,7 @@ class Env(MultiAgentEnv):
         self.outgoing_traffic_counts = {junc: 0 for junc in all_junction_list}
 
         self.incoming_vehicle_history = {junc: set() for junc in self.junction_list}
-        self.outgoing_vehicle_history = {junc: set() for junc in self.junction_list}
+        self.outgoing_vehicle_history = {junc: set() for junc in all_junction_list}
 
         # self.junction_vehicle_types = {junc_id: {'RL': 0, 'IDM': 0} for junc_id in self.junction_list}
         self.incoming_vehicle_types = {junc_id: {"RL": 0, "IDM": 0} for junc_id in self.junction_list}
@@ -1039,7 +1039,7 @@ class Env(MultiAgentEnv):
         self.incoming_traffic_counts = {junc: 0 for junc in self.junction_list}
         self.outgoing_traffic_counts = {junc: 0 for junc in all_junction_list}
         self.incoming_vehicle_history = {junc: set() for junc in self.junction_list}
-        self.outgoing_vehicle_history = {junc: set() for junc in self.junction_list}
+        self.outgoing_vehicle_history = {junc: set() for junc in all_junction_list}
         self.incoming_vehicle_types = {junc_id: {"RL": 0, "IDM": 0} for junc_id in self.junction_list}
         self.outgoing_vehicle_types = {junc_id: {"RL": 0, "IDM": 0} for junc_id in self.junction_list}
 
